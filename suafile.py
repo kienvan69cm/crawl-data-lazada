1a6a1aa9fe8f37044835370dce17ea0b8de18402
Lương Phúc Hậu <92417291+kienvan69cm@users.noreply.github.com>
2021-11-30 15:11:54 +0700

    Delete suafile.py

--- suafile.py
+++
@@ -1,6 +0,0 @@
-with open("data.csv", 'r', encoding='utf-8') as f:
-    datalist = f.readlines()
-for data in datalist:
-    with open("data2.csv", 'a', encoding="utf-8") as f:
-        dot = data.rfind(",")
-        f.write(data[:dot]+";"+data[dot+1:])
